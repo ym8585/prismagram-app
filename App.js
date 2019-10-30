e721f012770691c82726bd94d20ed4ab822a4795
ymlee <joo0171@naver.com>
2019-10-30 12:20:12 +0900

    .

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { AppLoading, Font, Asset } from "expo";
 import { Text, View, AsyncStorage } from "react-native";
